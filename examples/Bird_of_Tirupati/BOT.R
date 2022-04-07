3d29bba464c233839300f5dabc7f891d02d3cf43
Vinay K L <61734552+stachyris@users.noreply.github.com>
2022-04-07 12:37:25 +0530

    Create BOT.R

---
+++ examples/Bird_of_Tirupati/BOT.R
@@ -0,0 +1,25 @@
+library(sf)
+library(auk)
+library(dplyr)
+setwd("D:/IISER_Tirupati/Birds_of_Tirupati/Species_List")
+poly <- read_sf("BOT_shape.shp")
+f_out <- ("BOT.txt")
+auk_ebd("ebd_IN-AP-CH_prv_relApr-2021.txt") %>%
+auk_bbox(poly) %>%
+auk_complete() %>%
+auk_filter(f_out)
+ebd <- read_ebd("BOT.txt")
+ebd_sf <- ebd %>%
+  select(longitude, latitude) %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
+poly_ll <- st_transform(poly, crs = st_crs(ebd_sf))
+in_poly <- st_within(ebd_sf, poly_ll, sparse = FALSE)
+ebd_in_poly <- ebd[in_poly[, 1], ]
+
+
+par(mar = c(0,0,0,0))
+plot(poly %>% st_geometry(), col ="yellow", border = NA)
+plot(ebd_sf, col = "black", pch = 19, cex = 0.5, add = TRUE)
+plot(ebd_sf[in_poly[, 1], ],
+     col = "red", pch = 19, cex = 0.5, 
+     add = TRUE)
